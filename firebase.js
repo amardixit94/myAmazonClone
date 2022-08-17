fd254006ff5e9550666e542152f452069be5f7a5
Amar Dixit <86550850+amardixit94@users.noreply.github.com>
2022-08-17 09:14:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDgNGiC-6BEQx9NHPak8twSX9Bxx6PKVWk",
-    authDomain: "clone-770be.firebaseapp.com",
-    projectId: "clone-770be",
-    storageBucket: "clone-770be.appspot.com",
-    messagingSenderId: "1010145550676",
-    appId: "1:1010145550676:web:e956ded45a10efc8b7e8c1"
-  };
+// Your firebase config code.
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
